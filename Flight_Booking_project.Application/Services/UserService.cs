eec88282fb4298afadb9ec97c731d48fa9bc0d3a
SaiVivekpanchagiri <SaiVivekpanchagiri@DESKTOP-SQAUD5Q>
2024-09-13 19:42:22 +0530

    .

--- Flight_Booking_project.Application/Services/UserService.cs
+++ Flight_Booking_project.Application/Services/UserService.cs
@@ -70,12 +70,23 @@ public class UserService : IUserService
         var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
         var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-        var token = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], null,
-            expires: DateTime.Now.AddMinutes(10),
+        var claims = new List<Claim>
+    {
+        new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+    };
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
             signingCredentials: credentials
-            );
+        );
+
         return new JwtSecurityTokenHandler().WriteToken(token);
     }
+
 
     public async Task<string> LoginAsync(UserDto user)
     {
